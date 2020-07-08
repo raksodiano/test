e43c39fc764f86afc1652f3bd7c5a3e0ffa8034a
Oskar E. Torres O <oskar.torres.1234@gmail.com>
2020-07-08 14:02:44 -0400

    add cors and fix bugs

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
